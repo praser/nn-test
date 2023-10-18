92c490d40a9ea6fed28f95529818f5fe7fb6d1b7
Rubens Praser Junior <rubens.praser.junior@nn.nl>
2023-10-18 18:58:07 +0200

    Add comments

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
+/**
+ * Capitalizes the first letter of a string.
+ * @param word the word to capitalize
+ * @returns the capitalized word
+ */
 const capitalize = (word: string) =>
   word.charAt(0).toUpperCase() + word.slice(1)
 
+/**
+ * Capitalizes the first letter of each word in a string.
+ * @param input The string to title case.
+ * @param exclusion A space-separated list of words to exclude from title casing.
+ * @returns The title cased string.
+ */
 export const titleCase = (input: string, exclusion: string) => {
   const exclusions = exclusion.split(' ').map((w) => w.toLowerCase())
 
